ed1f33d8aeb897cb5c59bb87c2c3287e834d0253
Natália Melgaço <natalia.melgaco@gmail.com>
2023-02-27 17:29:24 -0300

    feat: adicionar conexao com o banco e cadastrar repositorio

--- src/router/repositorioGithubRouter.js
+++ src/router/repositorioGithubRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const listarRepositorioController = require('../controller/listarRepositorioController');
+const salvarRepositorioController = require('../controller/salvarRepositorioController');
 
 
-router.get('/buscarPerfil/github/:name', listarRepositorioController.listarPerfilGithub);
+router.get('/buscar/:name', listarRepositorioController.listarPerfilGithub);
+router.post('/salvar/:name', salvarRepositorioController.salvarPerfilGithub);
 
 
 module.exports = router;
