755cd79ca5667a33c433fcbf1db453875b2ecb48
Natália Melgaço <natalia.melgaco@gmail.com>
2023-02-27 17:43:11 -0300

    refactor: deletar arquivos antigos

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const router = require('./src/router/repositorioGithubRouter');
-
-app.use(router);
-
-app.listen(8080, function(req,res) {
-    console.log("servidor rodando na porta 8080");
-});
